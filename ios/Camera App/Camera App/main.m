94363d55a40c7b7b18f4eb80853ee41350a0c369
renxiaomo <mo_r@MacBook-Pro-de-mo-r.local>
2013-11-27 19:11:07 +0100

    camera app (take 2 photos, combine them and share on twitter)

---
+++ ios/Camera App/Camera App/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Camera App
+//
+//  Created by mo_r on 21/11/13.
+//  Copyright (c) 2013 mo_r. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CameraAppAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CameraAppAppDelegate class]));
+    }
+}
